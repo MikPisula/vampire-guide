23ac5861e2bf5d917b47e8d02885c2788c5e9d5d
MikPisula <MikPisula@users.noreply.github.com>
2024-06-22 20:41:29 +0200

    feat: this does not really work

--- showshadow.py
+++ showshadow.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+import math
+import numpy as np
 
 pygame.init()
 
@@ -12,7 +14,13 @@ BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-points = [(100, 100), (150, 200), (200, 150), (300, 300), (100, 300)]
+points = [(200, 200), (250, 300), (300, 250), (400, 400), (200, 400)]
+
+# def rotate2d(x, y, angle):
+#     cos_a = math.cos(angle)
+#     sin_a = math.sin(angle)
+#     return x * cos_a - y * sin_a, x * sin_a + y * cos_a
+
 
 while True:
     for event in pygame.event.get():
@@ -24,14 +32,20 @@ while True:
     
     sun_pos = pygame.mouse.get_pos()
 
-    pygame.draw.polygon(window, BLUE, points)
+    
+    # height = 10
+    # light_angle = math.pi/4
+    # shadow_length = math.atan(light_angle) * height
 
+    # shadow_points = points
     shadow_points = []
     for point in points:
-        shadow_point = (point[0] - sun_pos[0], point[1] - sun_pos[1]) # TODO: normalize this
-        shadow_points.append(shadow_point)    
+        vec = np.array([point[0] - sun_pos[0], point[1] - sun_pos[1]])
+        normalized_vec = (vec / np.linalg.norm(vec)) * 30
+        shadow_points.append((normalized_vec[0] + point[0], normalized_vec[1] + point[1]))    
     
     pygame.draw.polygon(window, BLACK, shadow_points)
     pygame.draw.circle(window, YELLOW, sun_pos, 20)
+    pygame.draw.polygon(window, BLUE, points)
 
     pygame.display.flip()

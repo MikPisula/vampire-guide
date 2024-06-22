af404db976ea75fa6de644059c85dd8990681ccb
Simon Hryszko <simon@hryszko.dev>
2024-06-22 21:48:07 +0200

    init search path form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,47 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section class="w-96">
+    <!-- row 1 -->
+    <div class="flex flex-row mb-4">
+      <div class="flex-1">
+          <input
+            type="text"
+            class="p-4 h-12 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            placeholder="Start longitude & latitude (comma separated)"
+            v-model="startLongitude">
+      </div>
+    </div>
+
+    <!-- row 2 -->
+    <div class="flex flex-row mb-4">
+      <div class="flex-1">
+          <input
+            type="text"
+            class="p-4 h-12 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" 
+            placeholder="End longitude & latitude (comma separated)"
+            v-model="endLongitude">
+      </div>
+    </div>
+
+    <!-- button -->
+    <div class="flex flex-row">
+      <a 
+        :href="ENDPOINT + '/test_augment?start=' + startLongitude + '&end=' + endLongitude"
+        target="_blank">
+        <button 
+          class="w-full py-2 px-4 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Search!
+        </button>
+      </a>
+    </div>
+  </section>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+
+import { ref, watch } from 'vue'
+
+const startLongitude = ref('40.7478495,-73.9843726')
+const endLongitude = ref('40.7466991,-73.9809522')
+
+const ENDPOINT = import.meta.env.VITE_API_ENDPOINT
+</script>

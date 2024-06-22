4ea5d229685534b1528c60133ebe75fe6410a5dc
Gergely Meszaros <meszaros.gergely97@gmail.com>
2024-06-22 22:44:15 +0200

    fix: Use great circle distance in shadow distance calculation

--- augment_graph.py
+++ augment_graph.py
@@ -2,24 +2,43 @@ import shapely
 import networkx as nx
 import functools
 import math
+import haversine
+import itertools
 
 from collections.abc import Callable
 
-def intersection_len_fun(G: nx.MultiDiGraph, polys: shapely.MultiPolygon, alpha: float = 5, beta: float = 5) -> Callable[[int, int, dict], float]:
+def greate_circle_length(line_strings: shapely.MultiLineString) -> float:
+    def single_line_str(line_string: shapely.LineString) -> float:
+        return sum(haversine.haversine(p1, p2, unit=haversine.Unit.METERS)
+                   for p1, p2 in itertools.pairwise(line_string.coords))
+
+    if not hasattr(line_strings, 'geoms'):
+        return single_line_str(line_strings)
+    
+    return sum(single_line_str(line_string) for line_string in line_strings.geoms)
+
+def intersection_len_fun(G: nx.MultiDiGraph, polys: shapely.MultiPolygon, alpha: float = 2, beta: float = 10) -> Callable[[int, int, dict], float]:
     @functools.cache
     def intersection_len_inner(u: int, v: int, edge_geom: shapely.LineString | None, len: float | None) -> float:
         if edge_geom is None:
             edge_geom = shapely.LineString([(G.nodes[u]['x'], G.nodes[u]['y']), (G.nodes[v]['x'], G.nodes[v]['y'])])
         if len is None:
-            len = edge_geom.length
+            return float('inf')
 
         intersection = edge_geom.intersection(polys)
-        return len  + alpha * math.exp((len - intersection.length) / beta)
+        int_len = greate_circle_length(intersection)
+        if int_len < 1e-4:
+            int_len = 0
+        val = len  + alpha * math.exp((len - int_len) / beta)
+        return val
     
     def intersection_len(u: int, v: int, data: dict) -> float:
-        edge_geom = data.get("geometry", None)
-        length = data.get("length", None)
-        return intersection_len_inner(u, v, edge_geom, length)
+        res = float('inf')
+        for val in data.values():
+            edge_geom = val.get("geometry", None)
+            length = val.get("length", None)
+            res = min(res, intersection_len_inner(u, v, edge_geom, length))
+        return res
 
     return intersection_len
 

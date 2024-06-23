ecf2e37a34565d85168b29265f18b836cf91c796
Gergely Meszaros <meszaros.gergely97@gmail.com>
2024-06-23 14:10:24 +0200

    fix: Handle when the data is not in memory in FooManager.get
    When FastAPI reloads memory gets cleaned, by saving metadata to disk
    we can keep working after forgeting requests after it.

--- area.py
+++ area.py
@@ -6,6 +6,8 @@ import shapely
 from dataclasses import dataclass
 import hashlib
 import networkx as nx
+import json
+from typing import Any
 from pydantic import BaseModel
 
 from pathlib import Path
@@ -27,6 +29,10 @@ class Area:
 
     def get_buildings(self) -> list[Polygon]:
         return to_polys(self.buildings['geometry'])
+    
+    @classmethod
+    def from_json(cls, data: dict[str, Any], buildings: gpd.GeoDataFrame, routing_network: nx.MultiDiGraph) -> 'Area':
+        return cls(data['id'], data['start'], data['end'], buildings, routing_network)
 
 class AreaManager:
     def __init__(self) -> None:
@@ -43,13 +49,15 @@ class AreaManager:
         return self.areas[id]
     
     def get(self, id: str) -> Area:
+        if id not in self.areas:
+            self.areas[id] = load_area(id)
         return self.areas[id]
 
 @dataclass
 class ShadowArea:
     id: str
     area_id: str
-    datetime: pd.Timestamp
+    time: pd.Timestamp
     shadows: gpd.GeoDataFrame
 
     def __post_init__(self) -> None:
@@ -57,6 +65,10 @@ class ShadowArea:
 
     def get_shadows(self) -> list[Polygon]:
         return to_polys(self.shadows_mp.geoms)
+    
+    @classmethod
+    def from_json(cls, data: dict[str, Any], shadows: gpd.GeoDataFrame) -> 'ShadowArea':
+        return cls(data['id'], data['area_id'], pd.Timestamp.fromisoformat(data['time']), shadows)
 
 class ShadowAreaManager:
     def __init__(self) -> None:
@@ -73,6 +85,8 @@ class ShadowAreaManager:
         return self.shadows[id]
     
     def get(self, id: str) -> ShadowArea:
+        if id not in self.shadows:
+            self.shadows[id] = load_shadow_area(id)
         return self.shadows[id]
 
 
@@ -88,9 +102,33 @@ def to_polys(geometry: pd.Series) -> list[Polygon]:
         result.append(Polygon(coords=inverted_coords, fill_color="#bbb", fill_opacity=1))
     return result
 
+def load_area(id: str) -> Area:
+    cache_prefix = CACHE_PATH / id[0] / id
+
+    area_meta = json.load(cache_prefix.with_name(f"area_meta_{cache_prefix.name}.json").open())
+    buildings = gpd.read_file(cache_prefix.with_name(f"buildings_{cache_prefix.name}.json"))
+    routing_network = ox.load_graphml(cache_prefix.with_name(cache_prefix.name + "_network.graphml"), edge_dtypes={'sun_length': float})
+    return Area.from_json(area_meta, buildings, routing_network)
+
+def load_shadow_area(id: str) -> ShadowArea:
+    cache_prefix = CACHE_PATH / id[0] / id
+
+    shadow_meta = json.load(cache_prefix.with_name(f"shadow_meta_{cache_prefix.name}.json").open())
+    shadows = gpd.read_file(cache_prefix.with_name(f"shadows_{cache_prefix.name}.json"))
+    return ShadowArea.from_json(shadow_meta, shadows)
+
 def prepare_area(id: str, start: tuple[float, float], end=tuple[float, float]) -> gpd.GeoDataFrame:
     cache_prefix = CACHE_PATH / id[0] / id
     cache_prefix.parent.mkdir(parents=True, exist_ok=True)
+
+    area_path = cache_prefix.with_name(f"area_meta_{cache_prefix.name}.json")
+    if area_path.exists():
+        area_meta = json.loads(area_path.read_text())
+        assert area_meta['start'] == start
+        assert area_meta['end'] == end
+    else:
+        area_meta = {"id": id, "start": start, "end": end}
+        json.dump(area_meta, area_path.open("w"))
 
     buildings_path = cache_prefix.with_name(f"buildings_{cache_prefix.name}.json")
     buildings: gpd.GeoDataFrame
@@ -113,16 +151,25 @@ def prepare_area(id: str, start: tuple[float, float], end=tuple[float, float]) -
 
     return Area(id, start, end, buildings, routing_network)
 
-def prepare_shadow_area(id: str, area: Area, datetime: pd.Timestamp) -> ShadowArea:
+def prepare_shadow_area(id: str, area: Area, time: pd.Timestamp) -> ShadowArea:
     cache_prefix = CACHE_PATH / id[0] / id
     cache_prefix.parent.mkdir(parents=True, exist_ok=True)
+
+    shadow_path = cache_prefix.with_name(f"shadow_meta_{cache_prefix.name}.json")
+    if shadow_path.exists():
+        shadow_meta = json.loads(shadow_path.read_text())
+        assert shadow_meta['area_id'] == area.id
+        assert pd.Timestamp.fromisoformat(shadow_meta['time']) == time
+    else:
+        shadow_meta = {"id": id, "area_id": area.id, "time": time.isoformat()}
+        json.dump(shadow_meta, shadow_path.open("w"))
 
     shadows_path = cache_prefix.with_name(f"shadows_{cache_prefix.name}.json")
     shadows: gpd.GeoDataFrame
     if shadows_path.exists():
         shadows = gpd.read_file(shadows_path)
     else:
-        shadows = calculate_polygon_shade(area.buildings, datetime)
+        shadows = calculate_polygon_shade(area.buildings, time)
         shadows_path.write_text(shadows.to_json())
 
-    return ShadowArea(id, area.id, datetime, shadows)
+    return ShadowArea(id, area.id, time, shadows)
